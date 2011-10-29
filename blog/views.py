8af29d27fd68028c479c93f95a18a0180c83ac9d
Ademaro <ademaro@z-gu.ru>
2011-10-30 03:25:29 +0400

    try/except for frong entry id

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.http import Http404
 from django.shortcuts import render_to_response
 from _tea.blog.models import Entry
 
@@ -7,6 +8,10 @@ def index(request):
     return render_to_response('index.html', {'entry_list': entry_list})
 
 def entry(request, entry_id):
-    entry = Entry.objects.get(id=int(entry_id))
-    tags = entry.category.all()
-    return render_to_response('entry.html', {'entry': entry, 'tags': tags})
+    try
+      entry = Entry.objects.get(id=int(entry_id))
+    except Entry.DoesNotExists:
+      raise Http404()
+    else:
+      tags = entry.category.all()
+      return render_to_response('entry.html', {'entry': entry, 'tags': tags})
