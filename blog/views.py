270dfda7bc10c0ba7955e5aee8b34b0d36f6b507
Ademaro <ademaro@z-gu.ru>
2011-11-01 05:17:41 +0400

    Revert "category added"
    This reverts commit 431bf74891518b8380b03479b2bcc701cfc22a10.

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.http import Http404
 from django.shortcuts import render_to_response
-from _tea.blog.models import Entry, Category
+from _tea.blog.models import Entry
 
 def index(request):
     entry_list = Entry.objects.all()
-    categories = Category.objects.all()
-    return render_to_response('index.html', {'entry_list': entry_list, 'categories': categories})
+    return render_to_response('index.html', {'entry_list': entry_list})
 
 def entry(request, entry_id):
     try:
       entry = Entry.objects.get(id=int(entry_id))
-    except Entry.DoesNotExist:
+    except Entry.DoesNotExists:
       raise Http404()
     else:
-      categories = Category.objects.all()
       tags = entry.category.all()
-      return render_to_response('entry.html', {'entry': entry, 'tags': tags, 'categories': categories})
-
-def tags(request, tag_id):
-    try:
-      tag = Category.objects.get(id=int(tag_id))
-    except Category.DoesNotExist:
-      raise Http404()
-    else:
-      entry_list = tag.entry_set.all()
-      categories = Category.objects.all()
-      return render_to_response('index.html', 
-	{'entry_list': entry_list, 'categories': categories, 'category': tag})
+      return render_to_response('entry.html', {'entry': entry, 'tags': tags})
--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
+# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from django.conf import settings
 admin.autodiscover()
@@ -10,9 +11,10 @@ urlpatterns = patterns('',
     # url(r'^_tea/', include('_tea.foo.urls')),
     url(r'^$', '_tea.blog.views.index'), 
     url(r'^entry/(\d+)/$', '_tea.blog.views.entry'), 
-    url(r'^tags/(\d+)/$', '_tea.blog.views.tags'), 
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}), 
 
+    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
