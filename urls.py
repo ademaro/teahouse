7c5f3143116753fa939dca25cff1cacc5ae719b5
unknown <teauser@ppc.(none)>
2011-11-06 03:50:46 +0400

    os specific bug

--- urls.py
+++ urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', '_tea.views.home', name='home'),
     # url(r'^_tea/', include('_tea.foo.urls')),
-    url(r'^$', '_tea.blog.views.index'), 
+    url(r'^$', 'blog.views.index'),
     url(r'^entry/(\d+)/$', '_tea.blog.views.entry'), 
     url(r'^tags/(\d+)/$', '_tea.blog.views.tags'), 
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}), 
