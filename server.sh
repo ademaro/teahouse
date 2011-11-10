ee28a22856bb98eb34bba0092a733c5ec3d05132
Ademaro <ademaro@z-gu.ru>
2011-11-10 23:52:57 +0400

    server.sh mod

--- server.sh
+++ server.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+PROJDIR="/var/www/z-gu.ru/_tea"
+PIDFILE="$PROJDIR/server.pid"
+
+cd $PROJDIR
+
 case "$1" in
  "start")
- ./manage.py runfcgi method=prefork host=127.0.0.1 port=8881 pidfile=/tmp/server.pid
+   exec python ./manage.py runfcgi method=prefork host=127.0.0.1 port=8881 pidfile=$PIDFILE
  ;;
  "stop")
- kill -9 `cat /tmp/server.pid`
+   kill -9 `cat -- $PIDFILE`
+   rm -f -- $PIDFILE
  ;;
  "restart")
- $0 stop
- sleep 1
- $0 start
+   $0 stop
+   sleep 1
+   $0 start
  ;;
- *) echo "Usage: ./server.sh {start|stop|restart}";;
+   *) echo "Usage: ./server.sh {start|stop|restart}";;
 esac
