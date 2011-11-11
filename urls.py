1a2c51f799265d806d4469e1e217e6136cd80eb8
Ademaro <ademaro@z-gu.ru>
2011-11-12 02:27:53 +0400

    nginx for static content

--- urls.py
+++ urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     url(r'^entry/(\d+)/$', '_tea.blog.views.entry'), 
     url(r'^tags/(\d+)/$', '_tea.blog.views.tags'), 
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/var/www/z-gu.ru/_tea/static'}),
-    url(r'mu-a0e9c771-fb62bfcb-31460fd2-f2daa98a.txt', 'django.views.static.serve', {'document_root': '/var/www/z-gu.ru/_tea/static'}),
+    #url(r'mu-a0e9c771-fb62bfcb-31460fd2-f2daa98a.txt', 'django.views.static.serve', {'document_root': '/var/www/z-gu.ru/_tea/static'}),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
